c7e11afa769d4246b4604e071aeda5d8d62e8792
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-12-19 17:27:33 +0545

    match any
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+static bool
+match_character(char pattern_char, char text_char) {
+    if (pattern_char == '.') {
+        return true;  /* . matches anything */
+    }
+
+    return pattern_char == text_char;
+}
+
+static bool
+match_pattern(const char *pattern, const char *text) {
+    for (int i = 0; pattern[i] != '\0'; i++) { /* foreach char in pattern */
+        if (text[i] == '\0') { /* ran out of text but still have pattern left */
+            return false;
+        }
+
+        if (!match_character(pattern[i], text[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int
 main(int argc, char *argv[]) {
@@ -8,10 +33,7 @@ main(int argc, char *argv[]) {
         return 1;
     }
 
-    char *pattern = argv[1];
-    char *text = argv[2];
-
-    if (strstr(text, pattern) != NULL) {
+    if (match_pattern(argv[1], argv[2])) {
         printf("match found!\n");
         return 0;
     } else {
